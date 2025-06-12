07d7d63d6eefbbd993ac93c839a8009068e4ecb9
Danimart0 <93148764+Danimart0@users.noreply.github.com>
2025-06-11 22:30:41 -0600

    Ruta users

--- inventario/urls.py
+++ inventario/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ProductoViewSet, CategoriaViewSet, ProveedorViewSet, HistorialViewSet
+from .views import ProductoViewSet, CategoriaViewSet, ProveedorViewSet, HistorialViewSet,  UsuarioViewSet
 
 router = DefaultRouter()
 router.register(r'productos', ProductoViewSet)
 router.register(r'categorias', CategoriaViewSet)
 router.register(r'proveedores', ProveedorViewSet)
 router.register(r'historial', HistorialViewSet)
-
+router.register(r'usuarios', UsuarioViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
